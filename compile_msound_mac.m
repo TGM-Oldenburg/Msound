41bf21aff5a165655420c9e7285e4675b491061e
Jan Willhaus <mail@janwillhaus.de>
2015-11-09 12:48:06 +0100

    OS X only supports Core Audio. No flag necessary in compile script.

--- compile_msound_mac.m
+++ compile_msound_mac.m
@@ -1,11 +1,9 @@
 function compile_msound_mac
 
 % Msound uses the open source library PortAudio which supports various
-% operating systems and audio APIs. For Mac systems the default audio
-% API is CoreAudio. CoreAudio is the default configuration of msound,
-% wich should compile on almost any MATLAB system using the installed
-% GCC compiler.
-bCoreAudio = true;
+% operating systems and audio APIs. Mac OS X is completely build around a
+% single audio API which is Core Audio. Therefore no selection of APIs
+% exists for the Mac.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('Building msound ...')
@@ -42,28 +40,22 @@ szFiles = addFile( szFiles, 'portaudio/src/os/unix/pa_unix_hostapis.c' );
 szFiles = addFile( szFiles, 'portaudio/src/os/unix/pa_unix_util.c'     );
 
 % Add PortAudio path for: CoreAudio
-if( bCoreAudio )
-    disp('    Using ''CoreAudio'' ...')
-    % Required defines to compile with CoreAudio support.
-    szDefines = addDefine( szDefines, 'PA_USE_COREAUDIO' );
-    % Trick the compiler into accepting the portaudio source code
-    szDefines = addDefine( szDefines, 'char16_t=UINT16_T');
-    % PortAudio
-    szPaths = addPath( szPaths, 'portaudio/src/hostapi/coreaudio'                 );
-    szFiles = addFile( szFiles, 'portaudio/src/hostapi/coreaudio/pa_mac_core_utilities.c' );
-    szFiles = addFile( szFiles, 'portaudio/src/hostapi/coreaudio/pa_mac_core_blocking.c' );
-    szFiles = addFile( szFiles, 'portaudio/src/hostapi/coreaudio/pa_mac_core.c' );
-    % Libs
-    szOptions = sprintf('%s %s', szOptions, 'LDFLAGS=''\$LDFLAGS -framework CoreFoundation -framework CoreServices -framework CoreAudio -framework AudioToolBox -framework AudioUnit''');
-end
+disp('    Using ''CoreAudio'' ...')
+% Required defines to compile with CoreAudio support.
+szDefines = addDefine( szDefines, 'PA_USE_COREAUDIO' );
+% Trick the compiler into accepting the portaudio source code
+szDefines = addDefine( szDefines, 'char16_t=UINT16_T');
+% PortAudio
+szPaths = addPath( szPaths, 'portaudio/src/hostapi/coreaudio'                 );
+szFiles = addFile( szFiles, 'portaudio/src/hostapi/coreaudio/pa_mac_core_utilities.c' );
+szFiles = addFile( szFiles, 'portaudio/src/hostapi/coreaudio/pa_mac_core_blocking.c' );
+szFiles = addFile( szFiles, 'portaudio/src/hostapi/coreaudio/pa_mac_core.c' );
+% Libs
+szOptions = sprintf('%s %s', szOptions, 'LDFLAGS=''\$LDFLAGS -framework CoreFoundation -framework CoreServices -framework CoreAudio -framework AudioToolBox -framework AudioUnit''');
+
 
 % Build msound MEX-file using default compiler.
 eval( [ 'mex', szOptions, szDefines, szPaths, szFiles, szLibs ] );
-
-
-% Call msound to see it doesn't crash... ;-)
-% msound
-
 
 
 function szCmd = addFile  ( szCmd, szFile )
